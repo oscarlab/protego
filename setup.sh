e03eddb99f6d0324124eb3fe08b2cdd58d0c0a8d
bpjain <bpjain@cs.stonybrook.edu>
2014-09-25 06:54:05 -0400

    Update the installation instructions

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ git clone https://github.com/oscarlab/protego.git
 
 #Kernel Compile and Install
 cd protego/linux-stable
-make menuconfig
+make menuconfig #Enable Security options  --->  Security hooks for protego system
 export CONCURRENCY_LEVEL=4
 make-kpkg clean
 make-kpkg --initrd kernel_image kernel_headers
@@ -23,19 +23,19 @@ cd proc_plugin
 sudo make helper
 cd ../shadow-4.1.5
 ./configure
-make plogin
+sudo make plogin
 cd ../xtables-addons-1.46
 ./configure
 make && sudo make install
 
 #Add the startup commands
 
-echo "iptables -A OUTPUT -j RAWSOCKET --allow icmp --icmp_type echo_request" > /etc/init.d/protego_startup.sh
-echo "iptables -A OUTPUT -j RAWSOCKET --deny_all" >> /etc/init.d/protego_startup.sh
+sudo echo "iptables -A OUTPUT -j RAWSOCKET --allow icmp --icmp_type echo_request" > /etc/init.d/protego_startup.sh
+sudo echo "iptables -A OUTPUT -j RAWSOCKET --deny_all" >> /etc/init.d/protego_startup.sh
 
-echo "nohup python $PWD/progs/daemon.py &" >> /etc/init.d/protego_startup.sh
-chmod a+x /etc/init.d/protego_startup.sh
-update-rc.d protego_startup.sh defaults 100
+sudo echo "nohup python $PWD/progs/daemon.py &" >> /etc/init.d/protego_startup.sh
+sudo chmod a+x /etc/init.d/protego_startup.sh
+sudo update-rc.d protego_startup.sh defaults 100
 
 #Reboot the system in protego kernel
 sudo reboot
